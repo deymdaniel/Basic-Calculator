0fbddf8c42f77dcde7a8e95f81211db55d20758a
daniel <danielbiacaniii@gmail.com>
2024-09-07 15:42:51 +0800

    Made number and function buttons clickable

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-const calculator = document.querySelector(".calculatorBody");
+const input = document.querySelector(".input");
+input.innerHTML = "";
 
-for (let i = 1; i < 19; i++) {
-  const createBtn = document.createElement("div");
-  createBtn.className(i);
-  calculator.appendChild(createBtn);
+const output = document.querySelector(".output");
+output.innerHTML = "0";
+
+const clear = document.querySelector(".clear");
+clear.onclick = function () {
+  input.innerHTML = "";
+};
+
+const numBtn = document.getElementsByClassName("num");
+
+for (let i = 0; i < numBtn.length; i++) {
+  numBtn[i].onclick = function () {
+    var btnVal = numBtn[i].innerHTML;
+
+    input.innerHTML += btnVal;
+  };
+}
+
+const funcBtn = document.getElementsByClassName("function");
+
+for (let i = 0; i < funcBtn.length; i++) {
+  funcBtn[i].onclick = function () {
+    var btnFunc = funcBtn[i].innerHTML;
+    if (input.innerHTML.includes(btnFunc)) {
+      input.innerHTML = 0;
+      input.innerHTML += btnFunc[i];
+    }
+    input.innerHTML += btnFunc;
+  };
 }
