ddc9152659f034b2444fc2cc796204357797d8e2
daniel <danielbiacaniii@gmail.com>
2024-09-07 22:23:47 +0800

    Redo the button functions

--- script.js
+++ script.js
@@ -4,30 +4,43 @@ input.innerHTML = "";
 const output = document.querySelector(".output");
 output.innerHTML = "0";
 
+var firstVal = 0;
+
+var operator;
+
+var secondVal = 0;
+
+//clear button
 const clear = document.querySelector(".clear");
 clear.onclick = function () {
   input.innerHTML = "";
 };
 
-const numBtn = document.getElementsByClassName("num");
-
-for (let i = 0; i < numBtn.length; i++) {
-  numBtn[i].onclick = function () {
-    var btnVal = numBtn[i].innerHTML;
-
-    input.innerHTML += btnVal;
-  };
+//number buttons
+function numClick() {
+  const numBtn = document.getElementsByClassName("num");
+  for (let i = 0; i < numBtn.length; i++) {
+    numBtn[i].onclick = function () {
+      var btnVal = numBtn[i].innerHTML;
+      input.innerHTML += btnVal;
+    };
+  }
 }
 
-const funcBtn = document.getElementsByClassName("function");
-
-for (let i = 0; i < funcBtn.length; i++) {
-  funcBtn[i].onclick = function () {
-    var btnFunc = funcBtn[i].innerHTML;
-    if (input.innerHTML.includes(btnFunc)) {
-      input.innerHTML = 0;
-      input.innerHTML += btnFunc[i];
-    }
-    input.innerHTML += btnFunc;
-  };
+//operation buttons
+function operationClick() {
+  const operationBtn = document.getElementsByClassName("operation");
+  for (let i = 0; i < operationBtn.length; i++) {
+    operationBtn[i].onclick = function () {
+      var operator = operationBtn[i].innerHTML;
+      input.innerHTML += operator;
+    };
+  }
 }
+
+function getFirstVal() {
+  numClick();
+  operationClick();
+}
+
+getFirstVal();
